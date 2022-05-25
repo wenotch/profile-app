35e633db9fcc9add66eed6115ffc560e6c4d228c
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-05-25 10:35:34 +0100

    initial flutter setup with card layout

---
+++ android/app/src/main/kotlin/nwanochie/dev/mobileapps/MainActivity.kt
@@ -0,0 +1,6 @@
+package nwanochie.dev.mobileapps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          children: <Widget> [
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              "Emmanuel ",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                fontSize: 28, fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Text(
+              "CURRENT NINJA LEVEL",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "8",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28, fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'emmanuelnwanochie247@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 16,
+                    letterSpacing: 1
+                  ),
+                )
+              ],
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        )
+      )
+    );
+  }
+}
